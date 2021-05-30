5b3b08e2bc793e7d60e35e0132b6add17a967983
Drake Scott <drakescott999@hotmail.com>
2021-05-30 13:43:19 -0400

    Initial commit

---
+++ src/main/java/org/example/taxes.java
@@ -0,0 +1,49 @@
+package org.example;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class taxes {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        //I know this is super inefficient I just wanted to practice with arrays in java.
+        double[] price;
+        price = new double[3];
+
+        double[] quant = new double[3];
+
+        double preTaxItems[];
+        preTaxItems = new double[3];
+
+        int i = 0;
+
+        //experimenting with arrays and while loops
+        while(i != 3){
+            System.out.print("Enter the price of item " + (i + 1) + ": ");
+            price[i] = input.nextDouble();
+
+            System.out.print("Enter the quantity of item " + (i + 1) + ": ");
+            quant[i] = input.nextDouble();
+
+            preTaxItems[i] = price[i] * quant[i];
+
+            i++;
+        }
+
+        double sum = 0;
+        for (int j = 0; j < price.length; j++)
+            sum += preTaxItems[j];
+
+        double taxesAdded = sum  * 0.055;
+        double afterTax = sum + taxesAdded;
+
+
+        //experimenting with different methods of formatting doubles.
+        System.out.printf("Subtotal: $%,.2f", sum);
+        System.out.printf("\nTax: $%,.2f", taxesAdded);
+
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        System.out.println("\nTotal: $" + df.format(afterTax));
+
+    }
+}
